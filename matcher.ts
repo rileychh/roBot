34123a1b9be7e409ab40c5587a8a089cf5b71aaa
Yi-Jyun Pan <me@pan93.com>
2025-04-05 13:28:42 +0800

    style: reformat

--- matcher.ts
+++ matcher.ts
@@ -1,8 +1,9 @@
-const matcher = /(?:不是|84|not)(?:[兔ㄊ2二🐰🐇]+|(\(2\)){2,}|two|bunny|rabbit)/u;
+const matcher =
+  /(?:不是|84|not)(?:[兔ㄊ2二🐰🐇]+|(\(2\)){2,}|two|bunny|rabbit)/u;
 
 export function containsBunny(message: string) {
   const normalizedMessage = message.normalize("NFKC");
-  const standardizedMessage = normalizedMessage.replace(/[()\s.,⋯！!]/ug, "");
+  const standardizedMessage = normalizedMessage.replace(/[()\s.,⋯！!]/gu, "");
 
   return matcher.test(standardizedMessage);
 }
